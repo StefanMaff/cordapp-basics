b84072e4bba529f4271f5bb704f0452ff8455366
StefanMaff <stefano.maffullo@gmail.com>
2017-08-03 13:15:31 +0200

    Working on a second CorDapp involving a Commercial Paper, push of Contract and State
    Signed-off-by: StefanMaff <stefano.maffullo@gmail.com>

--- kotlin-source/src/main/kotlin/com/template/contract/CommercialPaperContract.kt
+++ kotlin-source/src/main/kotlin/com/template/contract/CommercialPaperContract.kt
@@ -9,6 +9,12 @@ import com.template.state.CommercialPaperState
 import net.corda.contracts.asset.sumCashBy
 import net.corda.core.contracts.*
 import net.corda.core.crypto.SecureHash
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+import net.corda.core.node.services.VaultService
+import net.corda.core.transactions.TransactionBuilder
+import java.time.Instant
+import java.util.*
 
 //Implement the Contract interface or subclass an abstract class like OnLedgerAsset
 
@@ -167,5 +173,104 @@ class CommercialPaperContract : Contract {
 
 
     // Adding a Generation API to the contract
+    // Contract class MUST provide a verify function but they may also provide HELPER functions to simplify their usage. A simple class of functions most contracts provide are
+    // generation functions, which either create or modify  a transaction to perform a certain actions (usually an action is mappable 1:1 to a command but not always)
+
+    // Generation may involve complex logic, for example the cash contract has a generateSpend method that is given a set of cash states and choose a way to combine them together
+    // to satisfy the amount of money that is being sent. In the immutable state model that we are using, ledger entries (states) can only be created and deleted and never modified,
+    // therefore to spend 12$ when we have 9$ and 5$ we need to combine both outputs together, creating a 12$ to our creditor and 2$ back to ourselves
+    // In our commercial paper contract the things that can be done with it are pretty simple
+
+    //Lets start with a method to wrap up the issuance process:
+
+    // we take a reference that points to the issuing party (the caller) and which can contain any internal bookkeeping or reference number that we may require.
+    // The reference field is the ideal place to put (for example) a join key. Then the face value of the paper and the maturity date
+    // It returns a transaction builder, that is one of the few mutable classes that the platform provides. It allows you to add inputs,ouputs and commands to it
+    // and is designed to be passed around, potentially between multiple contracts
+
+    fun generateIssue(issuance: PartyAndReference, faceValue : Amount<Issued<Currency>>, maturityDate : Instant, notary : Party) : TransactionBuilder {
+
+        //The function we define create a CommercialPaperState object that mostly just use the arguments we are were given, but it fills out the owner field of the state
+        // with the same public key of the issuing party
+        val state = CommercialPaperState(issuance, issuance.party, faceValue, maturityDate)
+
+        // The returned partial transaction has a Command object as a parameter. This is a container for any objects that implements the commandData interface, along with a list
+        // of keys that are expected to sign this transaction. In this case issuance requires that the issuing party sign, so we put the key of the party here
+        // The transactionBuilder has a convenience withItems method that take a variable argument list. You can pass in any StateAndRef(input), ContractState(output) or commandObject
+        // and it will build the transaction for you
+        // In addition we ask the caller to select a notary that controls this state and prevents it from being double spent
+        return TransactionBuilder(notary = notary).withItems(state, Command(Commands.Issue(), issuance.party.owningKey))
+
+
+        // NB: Generation methods should ideally be written to compose with each other, i.e. they should take a TransactionBuilder as an argument instead of returning one
+        // Unless you are sure it doesn't make sense to combine this type of transaction with others.
+        // In this case, issuing a CP at the same time doing other things would just introduce complexity that isn't like to be worth it, so we return a fresh object each time
+
+    }
+
+    // Here the generateMove method takes a pre-existing TransactionBuilder and adds to it. This is correct because typically you will want to combine a sale of CP with the
+    // movements of some other asset, such as cash. So both generate methods should operate on the same transaction (an example of this is done in the Unit Test later)
+
+    //The paper is passed as a StateAndRef, a small object that has a copy  of a state object, and also the (txhash, index) that indicates the location of this state on the ledger
+    // We adde the existing paper as input, the same paper with the adjusted owner field as output, and finally a move command that has the old owner's public key: this is
+    // what forces the current ower's signature to be present on the transaction, and this is what is checked by the contrac
+    fun generateMove( tx: TransactionBuilder, paper: StateAndRef<CommercialPaperState>, newOwner : AbstractParty) {
+
+        tx.addInputState(paper)
+        tx.addOutputState(paper.state.data.withNewOwner(newOwner).second)
+        tx.addCommand(Command(Commands.Move(), paper.state.data.owner.owningKey))
+
+    }
+
+    @Throws (InsufficientBalanceException::class)
+    fun generateRedeem(tx: TransactionBuilder, paper: StateAndRef<CommercialPaperState>, vault: VaultService) {
+        // Add the cash movements using the states in our vault
+        // The vault is a concept that may be familiar from Bitcoin and Ethereum. It is a set of states (such as cash) that are owned by the caller
+        // Here we use the vault to update the partial transaction we are handed with a movement of cash from the issuer of the CP to the current owner
+        // If we don't have enough cash in our vault an exception is throw
+
+        vault.generateSpend(tx, paper.state.data.faceValue.withoutIssuer(), paper.state.data.owner)
+        // Here we see an example of composing contracts together. When an owner wishes to redeem the commercial paper, the issuer ( the caller ) must gather cash from its vault
+        // and send the face value to the owner of the paper
+
+        // Then we add the paper itself as an input
+        tx.addInputState(paper)
+        //Finally we add a redeem command that has to be signed by the owner of the commercial paper
+        tx.addCommand(Command(Commands.Redeem(), paper.state.data.owner.owningKey))
+
+    }
+
+    // A transaction builder is not by itself ready to be used anywhere, so first, we must convert it to something that is recognised by the network.
+    // The most important next step is for the participating entities to sign it
+    // Typically, an initiating flow will create an initial partially signed SignedTransaction by calling the serviceHub.signInitialTransaction method.
+    // Then the frozen SignedTransaction can be passed to other nodes by the flow, these can sign using serviceHub.createSignature and distribute.
+    // The CollectSignaturesFlow provides a generic implementation of this process that can be used as a subFlow.
+
+    /*
+    * Non-Asset-Oriented SmartContracts:
+    * It is proper to think of states as represeting of useful facts about the world, and code contracts as imposing logical relations on how facts combine
+    * to produce new facts. When writing a contract that handles deal-like entities rather than asset-like entries is better to refer to Interest-Rate-Swaps code
+    *
+    *
+    * Making Things Happen at a particular time:
+    * Ex: automatically redeem your commercial paper as soon as it matures. Corda provides a way for states to advertise scheduled events that should occur in the future.
+    * Learn more in the Event Scheduling tutorial
+    *
+    * ENCUMBRANCES:
+    *
+    * The encumbrance state, if present, forces additional controls over the encumbered state, since the encumbrance state contract will also be verified during
+    * the execution of the transaction.
+    * For example, a contract state could be encumbered with a time-lock contract state;
+    * the state is then only processable in a transaction that verifies that the time specified in the encumbrance time-lock has passed.
+    * The encumbered state refers to its encumbrance by index, and the referred encumbrance state is an output state in a particular position on the same transaction that c
+    * reated the encumbered state. Note that an encumbered state that is being consumed must have its encumbrance consumed in the same transaction,
+    * otherwise the transaction is not valid.
+    *
+    * CLAUSES:
+    *
+    * It is common for slightly different contracts to have lots of common logic that can be shared. For example, the concept of being issued, being exited and being upgraded
+    * are all usually required by any contract. Corda calls these frequently needed chunks of logic "clauses"
+    *
+    * */
 
 }
